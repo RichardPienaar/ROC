84cfc3bcd54d0b0dc449417fc9b403834b550feb
Richard <53608437+RichardPienaar@users.noreply.github.com>
2020-07-22 18:20:49 +1000

    Update ribbon.py
    updated notation / comments

--- ribbon.py
+++ ribbon.py
@@ -3,6 +3,9 @@ import numpy as np
 import csv, sys
 
 def parse(e_file):
+    '''
+    Returns x<float> and y<float> coordinates [xs], [ys] from a given excel file as produced by ROCCurve.py - save_csv(name,points)
+    '''
     xs =[]
     ys =[]
     if not 'base' in str(e_file):
@@ -20,27 +23,48 @@ def parse(e_file):
             i = 0
             for row in spamreader:
                 i+=1
-                if i %10000 == 0:
+                if i %100000 == 0:
                     xs.append(float(row[0]))
                     ys.append(float(row[1]))                        
     return xs,ys
 
-def parse_all(prefix):
+def parse_all(prefix, peaks=True):
+    '''
+    Parses all files with a given prefix <str> in one batch
+    peaks(bool) - True if parsing peak output files
+    return: all [([<float>,<float>,..],[<float>,<float>,..]),([<float>,<float>,..],[<float>,<float>,..])...]
+    '''
     all = []
     i = 0
 
     while True:
-        try:
-            file = prefix+str(i)+'.csv'       
-            all.append(parse(file))
-            print('woop', len(all[i][0]))
-            i+=1
-        except:
-            break
+        if peaks:
+            try:
+                file = prefix+'_'+str(i)+'Peaks.csv'       
+                all.append(parse(file))
+                i+=1
+            except:
+                break
+        else:
+            try:
+                file = prefix+'_'+str(i)+'Bases.csv'       
+                all.append(parse(file))
+                i+=1
+            except:
+                break
 
     return all
 
 def get_averages_and_errors(parsed):
+    '''
+    Averaging function for combining replicates of the same run
+    parsed <list> - output of parse_all(prefix)
+    returns:
+    Xs <[float,float,..]> horizontal steps to graph, either no. peaks or no. bases
+    average_Ys <[float,float,..]> corresponding mean values 
+    high_bounds <[float,float,..]> corresponding maximum y value for each x value
+    low_bounds <[float,float,..]> corresponding maximum x value for each y value
+    '''
     Xs = []
     average_Ys = []
     high_bounds = []
@@ -69,7 +93,9 @@ def get_averages_and_errors(parsed):
 
 
 def make_arrays(xs,average,low_bounds,high_bounds):
-    
+    '''
+    Transforms output of get_averages_and_errors(parsed) from lists to numpy arrays for use in ribbon(args,names)
+    '''
     x = np.array(xs, dtype=float)
     y = np.array(average, dtype=float)
     low =  np.array(low_bounds, dtype=float)
@@ -77,26 +103,57 @@ def make_arrays(xs,average,low_bounds,high_bounds):
 
     return x,y,low,high
 
-def ribbon(args):
+def ribbon(args,names):
+    '''
+    Graphs ROC curves (ribbon plots) 
+    args [<str>,<str>,..] the prefixes of each experimental group
+    names [<str>,<str>,..] the full names of each experimental group to display
+    '''
     averages = []
 
+    # Peaks
+    
     for arg in args:
-        to_plot = parse_all(str(arg))
+        to_plot = parse_all(str(arg),peaks=True)
         averages.append(get_averages_and_errors(to_plot))
-
+    
+    print('Graphing at peak level')
     fig, ax = plt.subplots()
+    ax.set_ylabel('True Positive Rate (Sensitivity)')
+    ax.set_xlabel('False Positive Rate (1 - Specificity)')
     for group in averages:
         x,y,low,high = make_arrays(group[0], group[1],group[2],group[3])
         ax.plot(x, y, '-')
         ax.fill_between(x, high, low, alpha=0.2)
+    
+    title = input('Please set a title for the graph - at the Peak Level')
+    ax.set_title(title)
+    fig.canvas.set_window_title(title)
+    plt.legend(names)
+    plt.show()
+    
+    # Bases
+
+    averages = []
+
+    print('Graphing at base level')
+    for arg in args:
+        to_plot = parse_all(str(arg),peaks=False)
+        averages.append(get_averages_and_errors(to_plot))
+
+
+    fig, ax = plt.subplots()
+    ax.set_ylabel('True Positive Rate (Sensitivity)')
+    ax.set_xlabel('False Positive Rate (1 - Specificity)')
+    for group in averages:
+        x,y,low,high = make_arrays(group[0], group[1],group[2],group[3])
+        ax.plot(x, y, '-')
+        ax.fill_between(x, high, low, alpha=0.2)
+    
+    title = input('Please set a title for the graph at the Base Level')
+    ax.set_title(title)
+    fig.canvas.set_window_title(title)
+    plt.legend(names)
 
     plt.show()
-
-def produce_ribbons(arguments):
-    prefixes = []
-    for arg in arguments:
-        prefixes.append(str(arg))
-    
-    ribbon(prefixes)
-    return
-    
+  
